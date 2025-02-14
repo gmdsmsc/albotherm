06a900cdecee32e6c38b37f440aac13832793521
gmdsmsc <graham2.macleod@live.uwe.ac.uk>
2025-02-14 11:09:57 +0000

    Removed plot title

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ def load_page(display_name):
     # Load data for Page 1
     df = load_table(tablename)
     sensor_columns = [col for col in df.columns if col != 'DateTime']
-    fig = px.line(df, x='DateTime', y=sensor_columns, title='Multiple Columns Line Plot')
+    fig = px.line(df, x='DateTime', y=sensor_columns)
     st.plotly_chart(fig)
 
 
