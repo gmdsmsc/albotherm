a603d680dade2c39c878c44dfc710485e12da6b8
gmdsmsc <graham2.macleod@live.uwe.ac.uk>
2025-02-17 22:01:37 +0000

    Removed shell script files

--- install.sh
+++
@@ -1,8 +0,0 @@
-
-chmod +x install.sh
-
-wget https://questdb.com/download/questdb-8.2.2-rt-linux-x86-64.tar.gz -O questdb.tar.gz
-
-tar -xvf questdb-8.2.2-rt-linux-x86-64.tar.gz -C $HOME/.questdb
-cd $HOME/.questdb
-./questdb.sh start
